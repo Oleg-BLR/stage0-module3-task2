88bf26aeebeca1661de10279895841fbc060a352
Oleg-BLR <81323776+Oleg-BLR@users.noreply.github.com>
2022-10-19 11:23:46 +0300

    Update CopiedNames.java

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -3,12 +3,16 @@ package lang.print.gaps.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-        int variable = 2000;
+        int secondVariable = 2000;
         int value = 1000;
-        int variable = 4000;
+        int thirdVariable = 4000;
         int value2 = 3000;
         int variables = 5000;
-        variables = 7000;
-        int value = 90000;
+        int secondVariables = 7000;
+        int value3 = 90000;
+        int intVariable = 100000;
+        int myInteger = 1000000;
+        int myVar = 1234567;
+        int mySecondInteger = 1234567;
     }
 }
